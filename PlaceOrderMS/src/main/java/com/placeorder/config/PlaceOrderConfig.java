d9a89b49f04142749a0d10d69bf200dad3c04499
Satish <Satish@DESKTOP-7DFQCU9>
2020-10-31 15:58:44 +0530

    PlaceOrderMS

---
+++ PlaceOrderMS/src/main/java/com/placeorder/config/PlaceOrderConfig.java
@@ -0,0 +1,58 @@
+package com.placeorder.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+public class PlaceOrderConfig implements WebMvcConfigurer{
+	public static final String INVENTORY_QUEUE = "MyInventory-Queue";
+	public static final String INVENTORY_EXCHANGE = "MyInventory-Exchange";
+	
+	private ApiInfo getMyApiInfo() {
+		return new ApiInfo( "PlaceOrderMS" , "Place Order Microserices",
+				"1.9","Free to use for 10 times",
+				new springfox.documentation.service.Contact("Satish Prasad","https://www.coursecube.com","sd@coursecube.com"),
+				"API Under Free Licence","https://www.coursecube.com" );
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+				.build( )
+				.apiInfo(getMyApiInfo() );
+	}
+	
+	@Bean(name = "myInventoryQueue")
+	Queue createInventoryQueue() {
+		return QueueBuilder.durable(INVENTORY_QUEUE).build();
+	}
+	
+	@Bean(name = "myInventoryExchange")
+	Exchange createInventoryExchange() {
+		return ExchangeBuilder.topicExchange(INVENTORY_EXCHANGE).build();
+	}
+	
+	@Bean
+	Binding inventoryBinding(Queue myInventoryQueue, TopicExchange myInventoryExchange) {
+		return BindingBuilder.bind(myInventoryQueue).to(myInventoryExchange).with(INVENTORY_QUEUE);
+	}
+}
---
+++ PlaceOrderMS/src/main/java/com/placeorder/dto/OrderInfo.java
@@ -0,0 +1,43 @@
+package com.placeorder.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.placeorder.entity.Order;
+import com.placeorder.entity.OrderItem;
+
+public class OrderInfo implements Serializable{
+	private Order order;
+	private List<OrderItem> itemsList;
+
+	public OrderInfo() {
+	}
+
+	public OrderInfo(Order order, List<OrderItem> itemsList) {
+		super();
+		this.order = order;
+		this.itemsList = itemsList;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public List<OrderItem> getItemsList() {
+		return itemsList;
+	}
+
+	public void setItemsList(List<OrderItem> itemsList) {
+		this.itemsList = itemsList;
+	}
+
+	@Override
+	public String toString() {
+		return order + ", " + itemsList;
+	}
+
+}

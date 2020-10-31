bd4f01cc1ffb9271d3eace5d1ccdd66ba472e06d
Satish <Satish@DESKTOP-7DFQCU9>
2020-10-31 15:59:28 +0530

    UserRatingsMS

---
+++ UserRatingMS/src/main/java/com/userratings/config/UserRatingsConfig.java
@@ -0,0 +1,59 @@
+package com.userratings.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+public class UserRatingsConfig implements WebMvcConfigurer{
+	public static final String RATINGS_QUEUE= "MyRatings-Queue";
+	public static final String RATINGS_EXCHANGE= "MyRatings-Exchange"; 
+	
+		private ApiInfo getMyApiInfo() {
+			return new ApiInfo( "UserRatingsMS" , "User Ratings Microservices",
+					"1.9","Free to use for 10 times",
+					new springfox.documentation.service.Contact("Satish Prasad","https://www.coursecube.com","sd@coursecube.com"),
+					"API Under Free Licence","https://www.coursecube.com" );
+		}
+
+		@Bean
+		public Docket api() {
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.paths(PathSelectors.any())
+					.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+					.build( )
+					.apiInfo(getMyApiInfo() );
+		}
+		
+		//RabbitMQ for Rating
+		@Bean(name = "myRatingsQueue")
+		Queue  createRatingsQueue() {
+			return QueueBuilder.durable(RATINGS_QUEUE).build();
+		}
+		
+		@Bean(name = "myRatingsExchange")
+		Exchange createRatingsExchange() {
+			return ExchangeBuilder.topicExchange(RATINGS_EXCHANGE).build();
+		}
+		
+		@Bean
+		Binding ratingBinding(Queue myRatingsQueue, TopicExchange myRatingsExchange) {
+			return BindingBuilder.bind(myRatingsQueue).to(myRatingsExchange).with(RATINGS_QUEUE);
+		}
+}
---
+++ UserRatingMS/src/main/java/com/userratings/service/RatingServiceImpl.java
@@ -0,0 +1,78 @@
+package com.userratings.service;
+
+import java.util.List;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.satish.rabbitmq.BookRatingInfo;
+import com.userratings.config.UserRatingsConfig;
+import com.userratings.dao.BookRatingDAO;
+import com.userratings.dao.UserRatingDAO;
+import com.userratings.entity.BookRating;
+import com.userratings.entity.UserRating;
+
+@Service
+public class RatingServiceImpl implements RatingService{
+	
+	@Autowired
+	private BookRatingDAO bookRatingDAO;
+	
+	@Autowired
+	private UserRatingDAO userRatingDAO;
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Override
+	public void addUserRating(UserRating userRating) {
+		//1. Add the User Rating
+		userRatingDAO.save(userRating); //BookId
+		
+		//2.Calculate the Avg rating for BookId
+		int bookId = userRating.getBookId();
+		List<UserRating> ratingList = userRatingDAO.getUserRatingByBookId(bookId);
+		
+		double sumRating = 0.0;
+		for(UserRating ur: ratingList) {
+			sumRating = sumRating+ur.getRating();
+		}
+		double avgRating = sumRating/ratingList.size();
+		
+		
+		//3.Update BookRating in UserRatingMS(Local)
+		BookRating bookRating = bookRatingDAO.findById(bookId).get();
+		bookRating.setAvgRating(avgRating);
+		bookRatingDAO.save(bookRating);
+		
+		//4. Update BookRating in BookSearchMS
+		//Send Message to RabbitMQ
+		BookRatingInfo bookRatingInfo = new BookRatingInfo();
+		bookRatingInfo.setBookId(bookRating.getBookId());
+		bookRatingInfo.setAvgRating(bookRating.getAvgRating());
+		bookRatingInfo.setNumberOfSearches(bookRating.getNumberOfSearches());
+		
+		rabbitTemplate.convertAndSend(UserRatingsConfig.RATINGS_QUEUE, bookRatingInfo);
+		
+	}
+
+	@Override
+	public List<UserRating> getUserRatingByUserId(String userId) {
+		return userRatingDAO.getUserRatingByUserId(userId);
+	}
+
+	@Override
+	public void updateBookRating(BookRating bookRating) {
+		bookRatingDAO.save(bookRating);
+	}
+
+	@Override
+	public BookRating getBookRatingByBookId(Integer bookId) {
+		BookRating bookRating = bookRatingDAO.getBookRatingByBookId(bookId);
+		return bookRating;
+	}
+	
+
+}
